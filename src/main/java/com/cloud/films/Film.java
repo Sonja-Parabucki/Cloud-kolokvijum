34da454dea3e8a948f004d98cb1d8a6a807453cb
Sonja-Parabucki <105170152+Sonja-Parabucki@users.noreply.github.com>
2024-04-13 18:58:04 +0200

    docker

--- src/main/java/com/cloud/films/Film.java
+++ src/main/java/com/cloud/films/Film.java
@@ -1,13 +1,12 @@
 package com.cloud.films;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Film {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
